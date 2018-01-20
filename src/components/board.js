020db6badbe885d4b937e97d14d3b6bec250ad6f
SamRC <samrc1337@gmail.com>
2018-01-20 19:20:53 +0000

    Added cell RULES

--- src/components/board.js
+++ src/components/board.js
@@ -5,6 +5,7 @@ import StateButton from './button';
 export default class Board extends Component{
   constructor(props){
     super(props);
+    
     const w = 10;
     const h = 10;
     const probAlive = .1;
@@ -17,12 +18,72 @@ export default class Board extends Component{
       height: h,
       prob: probAlive,
       board: board,
-    }
+      startGen:false,
+      tpointer:null,
+      genInterval:500,
+      genCount:0
+    };
     this.createBoard = this.createBoard.bind(this);
     this.clearBoard = this.clearBoard.bind(this);
     this.resetBoard = this.resetBoard.bind(this);
 
   }
+  
+  startGen=()=>{
+    console.log('in start gen');
+    this.setState({startGen:!this.state.startGen});
+    this.applyRules();
+    
+  };
+  
+  applyRules=()=>{
+    
+    if(!this.state.startGen){
+      let t=setInterval(()=>{
+        //console.log('genaration passed');
+        this.cellRules();
+      }, this.state.genInterval);
+      this.setState({tpointer:t});
+      //console.log('set tp');
+    }else{
+      //console.log('clear tp');
+      clearInterval(this.state.tpointer);
+    }
+  };
+  
+  cellRules=()=>{
+    let neighbours={};
+    let board_copy=this.state.board.slice();
+    for(let i=0; i<(this.state.width*this.state.height); i++){
+      neighbours=this.getNeighbours(i);
+      //console.log(neighbours["true"]);
+      if(board_copy[i]){
+        if(neighbours["true"]<2 || neighbours["true"]>3 || neighbours["true"]===undefined) 
+              board_copy[i]=false;
+        else if(neighbours["true"]===3 || neighbours["true"]===2) 
+              board_copy[i]=true;
+      }else{
+        if(neighbours["true"]===3) board_copy[i]=true;
+      }
+      
+    }
+    this.setState({board:board_copy});
+    
+  };
+  
+  getNeighbours=(i)=>{
+    let ne=[]
+     const board_copy=this.state.board.slice();
+     const w=this.state.width;
+     const ne_i=[i-1, i+1, i-w, i+w, i-w-1, i-w+1, i+w-1, i+w+1];
+    ne=ne_i.map(e=>board_copy[e]);
+    const ne_count={};
+    ne.forEach((e)=>{
+      ne_count[e]=(ne_count[e] || 0)+1
+    });
+    return ne_count;
+  };
+  
   clearBoard(){
     const clear = Array(this.state.width*this.state.height).fill(false);
     this.setState({
@@ -98,6 +159,11 @@ export default class Board extends Component{
     return(
       <div style={style}>
       {squares}
+      
+      <StateButton
+        onClick={this.startGen}
+        name={(this.state.startGen)? "Stop":"Start"}
+        />
       <StateButton
         onClick={this.clearBoard}
         name='Clear'
